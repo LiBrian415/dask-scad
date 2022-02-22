b88295ed4b90512d3743f87b9720039e95e4ae6a
Brian Li <librian415@gmail.com>
2022-02-21 18:55:22 -0800

    [1, gen] - Added output template test, Fixed template encoding, Added LFS processing

--- dask_scad/tests/test_template.py
+++ dask_scad/tests/test_template.py
@@ -1,3 +1,4 @@
+import base64
 import cloudpickle
 import json
 import os.path
@@ -29,6 +30,31 @@ def test_exec():
     _test_exec(add2_task, cache, cache['x'] + cache['y'] + 5)
 
 
+def test_output():
+    with tempfile.TemporaryDirectory() as td:
+        output = {'type': 'lfs', 'meta': {'dir': td}}
+        exp_cache = {'x': 1, 'y': 2, 'z': 3, ('a', 1): [1, 2, 3]}
+
+        template = get_template('output.j2')
+        with tempfile.NamedTemporaryFile(suffix='.py') as file:
+            with open(file.name, 'w') as f:
+                f.write(template.render(output=output))
+            res = json.loads(subprocess.run(['python3', file.name, base64.b64encode(cloudpickle.dumps(exp_cache)).decode('ascii')],
+                                            stdout=subprocess.PIPE).stdout.decode('utf-8'))
+            assert not res['error']
+            out = cloudpickle.loads(base64.b64decode(res['output']))
+            cache = dict()
+            for k, v in out.items():
+                with open(v, 'rb') as f:
+                    val = cloudpickle.loads(f.read())
+                cache[k] = val
+            assert len(cache) == len(exp_cache)
+            assert len(set(cache.keys()).difference(set(exp_cache.keys()))) == 0
+            assert len(set(exp_cache.keys()).difference(set(cache.keys()))) == 0
+            for k, v in exp_cache.items():
+                assert v == cache[k]
+
+
 # Jinja2 stuff
 TEMPLATE_PATHS = [os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")]
 
@@ -43,5 +69,6 @@ def get_template(name):
     return get_environment().get_template(name)
 
 
-# Run test
+# Run tests
 test_exec()
+test_output()
