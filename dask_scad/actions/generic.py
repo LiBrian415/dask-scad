3c8e58810177dce53d8ca2fe4cf767f32294bcea
Brian Li <librian415@gmail.com>
2022-03-07 11:21:15 -0800

    Revert "[1, gen] - fixed process bugs and added codegen"
    This reverts commit ae04faf0eda1ac13e6467661efbdae2208f092a5.

---
+++ dask_scad/actions/generic.py
@@ -0,0 +1,17 @@
+"""
+Generic SCAD action for function execution
+
+Note: tasks shouldn't modify data in-place
+"""
+
+def main(params, action):
+    """
+    Parameters
+    ----------
+    params : dict
+        A dictionary containing input params.
+    action :
+        Object used to access SCAD action APIs
+    """
+
+    return dict()
